d1e453cbe45732c65adc7a6d58fa9e5fd4985ca8
matthew-ferris <matthew_ferris@aol.com>
2021-11-26 11:37:48 -0600

    HELPPP

--- Website.R
+++
@@ -1,8 +0,0 @@
-#install necessary packages
-install.packages(c("distill","postcards"))
-
-#check version of these packages:
-packageVersion("distill")
-packageVersion("rmarkdown")
-packageVersion("postcards")
-
--- matthew-ferris.R
+++
@@ -1,9 +0,0 @@
-#To create the website?
-library(distill)
-library(postcards)
-create_website(dir = ".", title = "matthew-ferris", gh_pages = TRUE)
-
-#to add a postcard?
-create_postcard(file = "matthew.Rmd") # future name of .Rmd file
-
-create_theme("postcards")
